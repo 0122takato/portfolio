81895fdb25ee99e6d580328816e8951a46e2077f
0122takato <takato.buran0122@icloud.com>
2022-02-02 16:56:46 +0900

    ハンバーガーメニュー設定

--- js/index.js
+++ js/index.js
@@ -3,3 +3,15 @@ $("#particle").particleText({
     colors:["#fff","#fff", "#fff"], // パーティクルの色を複数指定可能
     speed: "slow",  // slow, middle, high の3つから粒子が集まる速さを選択
 });
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+});

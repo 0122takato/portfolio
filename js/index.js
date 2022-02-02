ff8aa7c65aea45a3c77c1c414111e7941a78ab1f
0122takato <takato.buran0122@icloud.com>
2022-02-02 16:38:47 +0900

    slowに変更

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $("#particle").particleText({
     text: "Takato's portfolio", // 表示させたいテキスト。改行の場合は<br>追加
     colors:["#fff","#fff", "#fff"], // パーティクルの色を複数指定可能
-    speed: "high",  // slow, middle, high の3つから粒子が集まる速さを選択
+    speed: "slow",  // slow, middle, high の3つから粒子が集まる速さを選択
 });
